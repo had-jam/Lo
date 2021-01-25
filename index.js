8f608bd78ae51ee295ac7a510e0533a67fc935c0
had-jam <hadjam@umich.edu>
2021-01-24 19:11:03 -0500

    Added a command.js file and commands into index.js such as server info, ping, and presence

--- index.js
+++ index.js
@@ -1,28 +1,65 @@
 // require the discord.js module
-const Discord = require('discord.js');
-const config = require('./config.json');
+const Discord = require('discord.js')
+const config = require('./config.json')
+const command = require('./command')
 
 // create the Discord client
-const client = new Discord.Client();
+const client = new Discord.Client()
 
 // When the app is ready it will write out Ready using this
 client.once('ready', () => {
-    console.log('Ready!');
+    console.log('Ready!')
+
+    command(client, ['ping', 'test'], (message) => {
+        message.channel.send('Pong!')
+    })
+
+    command(client, 'servers', message => {
+        client.guilds.cache.forEach(guild => {
+            message.channel.send(
+                `${guild.name} has a total of ${guild.memberCount} members`
+            )
+        })
+    })
+
+    command(client, 'server', message => {
+            message.channel.send(
+                `${message.guild.name} has a total of ${message.guild.memberCount} members`
+            )
+    })
 
     client.user.setPresence({
         activity: {
             name: 'Ro watch Lynn',
             type: 3,
         },
-    });
-});
+    })
 
-client.on('message', message => {
-    if (message.content === '!Hello') {
-        // send back "Pong." to the channel the message was sent in
-        message.channel.send('hello to you too.');
-    }
-});
+    command(client, 'status', (message) => {
+        if (message.author.id === '128346198917644290') {
+            const content = message.content.replace('!status ', '')
+
+        client.user.setPresence({
+            activity: {
+                name: content,
+                type: 0,
+            },
+        })
+        }
+        else {
+            message.channel.send('Only Malek can change my status')
+        }
+    })
+
+    command(client, ['cc', 'clearchannel'], (message) => {
+        if (message.member.hasPermission('ADMINISTRATOR')) {
+            message.channel.messages.fetch().then((results) => {
+                message.channel.bulkDelete(results)
+            })
+        }
+    })
+
+})
 
 // Replace the value between the quotes with your token
 client.login(config.token);
